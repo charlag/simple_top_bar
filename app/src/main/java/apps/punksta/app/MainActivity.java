f2cd2b8226a0a580ee93a3f160301df9815c874d
Станислав Шакиров <skinnystas@gmail.com>
2016-01-26 11:56:31 +0300

    Added new demo app

--- app/src/main/java/apps/punksta/app/MainActivity.java
+++ app/src/main/java/apps/punksta/app/MainActivity.java
@@ -1,61 +1,66 @@
 package apps.punksta.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.util.Arrays;
 
 import apps.punksta.openactionbar.DrawableActon;
 import apps.punksta.openactionbar.IActionBar;
+import apps.punksta.openactionbar.Styles;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Styles.Gravity gravity = Styles.Gravity.center;
+    private Styles.ViewType viewType = Styles.ViewType.icon;
+    private TextView status;
+    IActionBar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        IActionBar toolbar = (IActionBar) findViewById(R.id.toolbar);
+        toolbar = (IActionBar) findViewById(R.id.toolbar);
+        toolbar.setViewType(viewType);
+        toolbar.setGravity(gravity);
+
+        toolbar.getTitle().setText("test");
+
 
         toolbar.setActions(Arrays.asList(
                 new DrawableActon(123, R.drawable.ic_menu_black_24dp, "123"),
                 new DrawableActon(123, android.R.drawable.bottom_bar,  "123")
         ), false);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        findViewById(R.id.changeGravity).setOnClickListener(this);
+        findViewById(R.id.changeViewType).setOnClickListener(this);
+
+        status = (TextView) findViewById(R.id.status);
+        updateStatus();
+    }
+
+    <E extends Enum<E>> E getNext(E current) {
+        E[] values = (E[]) current.getClass().getEnumConstants();
+        int size = values.length;
+        return values [ (current.ordinal() + 1) % size];
+    }
+
+    void updateStatus() {
+        status.setText(gravity.toString() + " " + viewType.toString());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.changeGravity:
+                gravity = getNext(gravity);
+                toolbar.setGravity(gravity);
+                break;
+            case R.id.changeViewType:
+                viewType = getNext(viewType);
+                toolbar.setViewType(viewType);
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
+        updateStatus();
     }
 }
