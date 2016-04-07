0ec8800a40c39503615895ab1228f8d4cce30203
punksta <skinnystas@gmail.com>
2016-04-07 15:15:53 +0300

    fix dimenstions, add public setters

--- open_action_bar/src/main/java/apps/punksta/openactionbar/SimpleTopBar.java
+++ open_action_bar/src/main/java/apps/punksta/openactionbar/SimpleTopBar.java
@@ -46,6 +46,44 @@ public class SimpleTopBar extends RelativeLayout implements
         paintDrawableActionsToTitleColor = true;
     }
 
+    public SimpleTopBar(Context context) {
+        super(context);
+        init();
+        menu = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_menu);
+        title = (TextView) findViewById(apps.punksta.openactionbar.R.id.action_bar_title);
+        appIcon = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_app_icon);
+        actionsLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_actions);
+        titleLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_title_layout);
+    }
+
+    public SimpleTopBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+        menu = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_menu);
+        title = (TextView) findViewById(apps.punksta.openactionbar.R.id.action_bar_title);
+        actionsLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_actions);
+        appIcon = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_app_icon);
+        titleLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_title_layout);
+
+        parseAtr(attrs, 0);
+    }
+
+    public SimpleTopBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+        menu = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_menu);
+        title = (TextView) findViewById(apps.punksta.openactionbar.R.id.action_bar_title);
+        actionsLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_actions);
+        appIcon = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_app_icon);
+        titleLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_title_layout);
+
+        parseAtr(attrs, defStyleAttr);
+    }
+
+    public static int pxFromDp(final Context context, final float dp) {
+        return (int) (dp * context.getResources().getDisplayMetrics().density);
+    }
+
     private void parseAtr(AttributeSet set, int defStyleAttr) {
         TypedArray a = getContext().getTheme().obtainStyledAttributes(
                 set,
@@ -102,33 +140,6 @@ public class SimpleTopBar extends RelativeLayout implements
         return actionsLayout;
     }
 
-    @Override
-    public void setTitleGravity(Styles.TitleGravity titleGravity) {
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        switch (titleGravity) {
-            case left:
-                params.addRule(RelativeLayout.END_OF, apps.punksta.openactionbar.R.id.action_bar_menu);
-                params.removeRule(RelativeLayout.CENTER_HORIZONTAL);
-                params.addRule(RelativeLayout.START_OF, R.id.action_bar_actions);
-                params.width = title.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
-                break;
-            case center:
-                params.removeRule(RelativeLayout.END_OF);
-                params.removeRule(RelativeLayout.START_OF);
-                params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
-                params.width = title.getLayoutParams().width = ViewGroup.LayoutParams.WRAP_CONTENT;
-
-                int maxWidth = Math.max(actionsLayout.getWidth(), menu.getWidth());
-                params.leftMargin = params.rightMargin = maxWidth;
-
-                break;
-        }
-
-        titleLayout.setLayoutParams(params);
-        this.titleGravity = titleGravity;
-        updateMargins();
-    }
-
     void updateMargins() {
         if (titleGravity == null)
             return;
@@ -152,6 +163,82 @@ public class SimpleTopBar extends RelativeLayout implements
         }
         appIcon.setLayoutParams(imageParams);
         titleLayout.setLayoutParams(params);
+    }
+
+    @Override
+    public void setTitleColor(int color) {
+        title.setTextColor(color);
+        PorterDuffColorFilter colorFilter = new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        menu.setColorFilter(colorFilter);
+        if (paintDrawableActionsToTitleColor)
+            paintActions(color);
+    }
+
+    private void paintActions(int color) {
+        for (View view: actions) {
+            if (view.getTag() instanceof DrawableActon) {
+                Drawable d = ((ImageView)view).getDrawable();
+                if (d != null)
+                    d.mutate().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+    }
+
+    @Override
+    public final void setBackgroundColor(int color) {
+        super.setBackgroundColor(color);
+    }
+
+    private void init() {
+        horisontalMargin = (int) getContext().getResources().getDimension(R.dimen.open_action_bar_horizontal_margin);
+        maxIconWidth = (int) getContext().getResources().getDimension(R.dimen.open_action_bar_image_max_width);
+        maxIconWidthWithoutTitle = (int) getContext().getResources().getDimension(R.dimen.open_action_bar_image_max_width_without_title);
+
+
+        LayoutInflater.from(getContext()).inflate(apps.punksta.openactionbar.R.layout.action_bar_layout, this, true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            float elevation = pxFromDp(getContext(), 4);
+            setElevation(elevation);
+        }
+    }
+
+    public void setMaxIconWidthWithoutTitle(int maxIconWidthWithoutTitle) {
+        this.maxIconWidthWithoutTitle = maxIconWidthWithoutTitle;
+    }
+
+    public void setMaxIconWidth(int maxIconWidth) {
+        this.maxIconWidth = maxIconWidth;
+    }
+
+    public void setHorisontalMargin(int horisontalMargin) {
+        this.horisontalMargin = horisontalMargin;
+    }
+
+    @Override
+    public void setActions(List<? extends Action> buttons, boolean sameColorWithTitle) {
+        this.actions.clear();
+        this.actionsLayout.removeAllViews();
+        List<View> result = ActionBuilder.fillLayout(getContext(), buttons, actionsLayout);
+        actions.addAll(result);
+        for (View v : result) {
+            v.setOnClickListener(this);
+        }
+        if (sameColorWithTitle)
+            paintActions(title.getCurrentTextColor());
+    }
+
+    @Override
+    public View getView(Action action) {
+        for (View view : actions) {
+            if (view.getTag().equals(action))
+                return view;
+        }
+        throw new IllegalArgumentException(action + " is not added to actions");
+    }
+
+    @Override
+    public Styles.ViewType getViewType() {
+        return viewType;
     }
 
     public void setViewType(Styles.ViewType viewType) {
@@ -196,112 +283,36 @@ public class SimpleTopBar extends RelativeLayout implements
         updateMargins();
     }
 
-
-    @Override
-    public void setTitleColor(int color) {
-        title.setTextColor(color);
-        PorterDuffColorFilter colorFilter = new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-        menu.setColorFilter(colorFilter);
-        if (paintDrawableActionsToTitleColor)
-            paintActions(color);
-    }
-
-    private void paintActions(int color) {
-        for (View view: actions) {
-            if (view.getTag() instanceof DrawableActon) {
-                Drawable d = ((ImageView)view).getDrawable();
-                if (d != null)
-                    d.mutate().setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-            }
-        }
-    }
-
-    @Override
-    public final void setBackgroundColor(int color) {
-        super.setBackgroundColor(color);
-    }
-
-    public SimpleTopBar(Context context) {
-        super(context);
-        init();
-        menu = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_menu);
-        title = (TextView) findViewById(apps.punksta.openactionbar.R.id.action_bar_title);
-        appIcon = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_app_icon);
-        actionsLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_actions);
-        titleLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_title_layout);
-    }
-
-    public SimpleTopBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-        menu = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_menu);
-        title = (TextView) findViewById(apps.punksta.openactionbar.R.id.action_bar_title);
-        actionsLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_actions);
-        appIcon = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_app_icon);
-        titleLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_title_layout);
-
-        parseAtr(attrs, 0);
-    }
-
-    public SimpleTopBar(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-        menu = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_menu);
-        title = (TextView) findViewById(apps.punksta.openactionbar.R.id.action_bar_title);
-        actionsLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_actions);
-        appIcon = (ImageView) findViewById(apps.punksta.openactionbar.R.id.action_bar_app_icon);
-        titleLayout = (LinearLayout) findViewById(apps.punksta.openactionbar.R.id.action_bar_title_layout);
-
-        parseAtr(attrs, defStyleAttr);
-    }
-
-    private void init() {
-        horisontalMargin = (int) getContext().getResources().getDimension(R.dimen.open_action_bar_horizontal_margin);
-        maxIconWidth = (int) getContext().getResources().getDimension(R.dimen.open_action_bar_image_max_width);
-        maxIconWidthWithoutTitle = (int) getContext().getResources().getDimension(R.dimen.open_action_bar_image_max_width_without_title);
-
-
-        LayoutInflater.from(getContext()).inflate(apps.punksta.openactionbar.R.layout.action_bar_layout, this, true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            float elevation = pxFromDp(getContext(), 4);
-            setElevation(elevation);
-        }
-    }
-
-    public static int pxFromDp(final Context context, final float dp) {
-        return (int) (dp * context.getResources().getDisplayMetrics().density);
-    }
-
-    @Override
-    public void setActions(List<? extends Action> buttons, boolean sameColorWithTitle) {
-        this.actions.clear();
-        this.actionsLayout.removeAllViews();
-        List<View> result = ActionBuilder.fillLayout(getContext(), buttons, actionsLayout);
-        actions.addAll(result);
-        for (View v: result) {
-            v.setOnClickListener(this);
-        }
-        if (sameColorWithTitle)
-            paintActions(title.getCurrentTextColor());
-    }
-
-    @Override
-    public View getView(Action action) {
-       for (View view : actions) {
-           if (view.getTag().equals(action))
-               return view;
-       }
-        throw new IllegalArgumentException(action + " is not added to actions");
-    }
-
-    @Override
-    public Styles.ViewType getViewType() {
-        return viewType;
-    }
-
     @Override
     public Styles.TitleGravity getTitleGravity() {
         return titleGravity;
+    }
+
+    @Override
+    public void setTitleGravity(Styles.TitleGravity titleGravity) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        switch (titleGravity) {
+            case left:
+                params.addRule(RelativeLayout.END_OF, apps.punksta.openactionbar.R.id.action_bar_menu);
+                params.removeRule(RelativeLayout.CENTER_HORIZONTAL);
+                params.addRule(RelativeLayout.START_OF, R.id.action_bar_actions);
+                params.width = title.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+                break;
+            case center:
+                params.removeRule(RelativeLayout.END_OF);
+                params.removeRule(RelativeLayout.START_OF);
+                params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+                params.width = title.getLayoutParams().width = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+                int maxWidth = Math.max(actionsLayout.getWidth(), menu.getWidth());
+                params.leftMargin = params.rightMargin = maxWidth;
+
+                break;
+        }
+
+        titleLayout.setLayoutParams(params);
+        this.titleGravity = titleGravity;
+        updateMargins();
     }
 
     @Override
